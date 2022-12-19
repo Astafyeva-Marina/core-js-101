197bf87eab2f7afa914cc26b75ea4ae3238db661
Alexey Sidorov <astafyeva.marina@mail.ru>
2022-12-20 00:52:11 +0400

    fix: 02-numbers-tasks.js

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -242,7 +242,7 @@ function isPrime(n) {
  *   toNumber(new Number(42), 0) => 42
  */
 function toNumber(value, def) {
-  return Number(value) || def;;
+  return Number(value) || def;
 }
 
 module.exports = {
